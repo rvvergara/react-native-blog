f09a2f81e0b458d0a6cff4eda9bcf7168834b95d
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-10 16:19:03 +0800

    Create IndexScreen and connect to App

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({});
+
+const IndexScreen = () => (
+  <View>
+    <Text>This is Index Screen</Text>
+  </View>
+);
+
+export default IndexScreen;
