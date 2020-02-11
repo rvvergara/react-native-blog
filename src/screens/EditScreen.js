6981ece2c68edce24e1633e75beb393e0dc78715
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-11 17:50:36 +0800

    Fix navigation to Edit post screen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,17 +1,23 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {withNavigation} from 'react-navigation';
+import { View, Text, StyleSheet } from 'react-native';
+import { withNavigation } from 'react-navigation';
 import PostForm from '../components/PostForm';
 
 const styles = StyleSheet.create({});
 
-const EditScreen = ({navigation}) => {
+const EditScreen = ({ navigation }) => {
   const post = navigation.getParam('post');
 
   return (
     <View>
-      <Text>Edit {post.title} here</Text>
-      <PostForm post={post} origin={'Post'} />
+      <Text>
+Edit
+        {' '}
+        {post.title}
+        {' '}
+here
+      </Text>
+      <PostForm post={post} origin="Post" />
     </View>
   );
 };
--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,27 +1,25 @@
 import React from 'react';
-import {View, StyleSheet, TouchableOpacity} from 'react-native';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Post from '../components/Post';
 
 const styles = StyleSheet.create({});
 
-const PostScreen = ({navigation}) => (
+const PostScreen = ({ navigation }) => (
   <View>
     <Post post={navigation.getParam('post')} />
   </View>
 );
 
-PostScreen.navigationOptions = ({navigation}) => {
-  return {
-    headerRight: () => (
-      <TouchableOpacity
-        onPress={() =>
-          navigation.navigate('Edit', {post: navigation.getParam('post')})
-        }>
-        <Icon name="pencil" size={30} />
-      </TouchableOpacity>
-    ),
-  };
-};
+PostScreen.navigationOptions = ({ navigation }) => ({
+  headerRight: () => (
+    <TouchableOpacity
+      onPress={() => navigation.push('Edit', { post: navigation.getParam('post') })
+          }
+    >
+      <Icon name="pencil" size={30} />
+    </TouchableOpacity>
+  ),
+});
 
 export default PostScreen;
