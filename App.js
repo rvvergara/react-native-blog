2845deb1295d23e216ee940e93b68d10d39db04d
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-11 12:01:52 +0800

    Add CreateScreen

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
 import IndexScreen from './src/screens/IndexScreen';
 import PostScreen from './src/screens/PostScreen';
+import CreateScreen from './src/screens/CreateScreen';
 import {Provider} from './src/context/BlogContext';
 
 const navigator = createStackNavigator(
   {
     Index: IndexScreen,
     Post: PostScreen,
+    Create: CreateScreen,
   },
   {
     initialRouteName: 'Index',
---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({});
+
+const CreateScreen = () => (
+  <View>
+    <Text>Add New Post Here</Text>
+  </View>
+);
+
+export default CreateScreen;
